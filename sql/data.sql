a2c448d4227405e79a4b5597168f11c9f5b8ccc5
rhiodam <rhioda.m@gmail.com>
2019-03-02 06:57:29 +0700

    Create data.sql

---
+++ sql/data.sql
@@ -0,0 +1,12 @@
+
+use java_jdbc_basics;
+
+create table account (id int , accno int , lastname varchar(25) , firstname varchar(25) , bal int);
+
+select * from account;
+
+use mydb;
+
+create table user (firstName varchar(20),lastName varchar(20), email varchar(20),password varchar(20));
+
+select * from user;
