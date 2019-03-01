4de88719abe9c461c6eb0598d946309026a18417
rhiodam <rhioda.m@gmail.com>
2019-03-02 03:53:00 +0700

    Update AccountDAO.java

--- JDBCBasics/src/com/rhiodamuthie/jdbc/dao/AccountDAO.java
+++ JDBCBasics/src/com/rhiodamuthie/jdbc/dao/AccountDAO.java
@@ -14,14 +14,30 @@ public class AccountDAO {
 		String user = "root";
 		String pass ="";
 		
+		
+		
 		try {
 			Connection connection = DriverManager.getConnection(url, user, pass);
+			Statement statement = connection.createStatement();
 			System.out.println(connection);
 			
-			Statement statement = connection.createStatement();
-			int result = statement.executeUpdate("insert into account values (1,123231231,'muthie' , 'rhioda' , 100000)");
-			System.out.println(result + " row has been inserted..");
+//			Statement statement = connection.createStatement();
+//			int result = statement.executeUpdate("insert into account values (1,123231231,'muthie' , 'rhioda' , 100000)");
+//			System.out.println(result + " row has been inserted..");			
 			
+//			int result = statement.executeUpdate("update account set bal=100000000 where id =1");
+//			System.out.println(result + " row has been updated..");
+			
+			
+//			int result = statement.executeUpdate("dalete from account where id =1");
+//			System.out.println(result + " row has been updated..");
+			
+//			Data generation
+			for ( int i =0 ; i<100 ; i++) {
+				int sum = statement.executeUpdate("insert into account values ({0},123231231,'muthie' , 'rhioda' , 100000)" ,i);
+				System.out.println(sum + " row has been inserted..");
+			}
+	
 			
 			
 		} catch (SQLException e) {
